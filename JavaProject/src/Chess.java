283c13d5a79cdebc46dd919cf7aac70d0ec6e5da
vishwanath96 <vishwanath.patil1996@gmail.com>
2018-11-29 20:57:08 +0530

    added some pieces

--- JavaProject/src/Chess.java
+++ JavaProject/src/Chess.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 public class Chess extends JFrame {
   JLayeredPane pane;
   JPanel board;
+  JLabel piece;
  
   public Chess(){
   Dimension bSize = new Dimension(600, 600);
@@ -33,5 +34,53 @@ public class Chess extends JFrame {
   else
   square.setBackground( i % 2 == 0 ? Color.white : Color.black );
   }
+  JLabel piece = new JLabel( new ImageIcon("/Users/Vishu/Desktop/chesscoins/brook.png") );
+  JPanel panel = (JPanel)board.getComponent(0);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bknight.png"));
+  panel = (JPanel)board.getComponent(1);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bbishop.png"));
+  panel = (JPanel)board.getComponent(2);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bqueen.png"));
+  panel = (JPanel)board.getComponent(4);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bking.png"));
+  panel = (JPanel)board.getComponent(3);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bbishop.png"));
+  panel = (JPanel)board.getComponent(5);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bknight.png"));
+  panel = (JPanel)board.getComponent(6);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/brook.png"));
+  panel = (JPanel)board.getComponent(7);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(8);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(9);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(10);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(11);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(12);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(13);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(14);
+  panel.add(piece);
+  piece = new JLabel(new ImageIcon("/Users/Vishu/Desktop/chesscoins/bpawn.png"));
+  panel = (JPanel)board.getComponent(15);
+  panel.add(piece);
  
 }}
